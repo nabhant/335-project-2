d1fc2274c03c4d6c84362399b2c3b08ffe1079ba
nabhant <nt142003@gmail.com>
2023-12-12 18:40:24 -0500

    Debug merge

--- MergeSort.hpp
+++ MergeSort.hpp
@@ -3,23 +3,26 @@
 #include <chrono> 
 #include <algorithm>
 
-void mergeSortRecursive(const std::vector<int>& nums, std::vector<int>& sorted) {
+void recursiveCall(std::vector<int>& nums, std::vector<int>& sorted) {
     int size = nums.size();
 
     if (size <= 1) {
         sorted = nums;  
         return;
     }
-
     int middle = size / 2;
     std::vector<int> left(nums.begin(), nums.begin() + middle);
     std::vector<int> right(nums.begin() + middle, nums.end());
     std::vector<int> sortedLeft, sortedRight;
 
-    mergeSortRecursive(left, sortedLeft);
-    mergeSortRecursive(right, sortedRight);
+    recursiveCall(left, sortedLeft);
+    recursiveCall(right, sortedRight);
     sorted.clear(); 
     std::merge(sortedLeft.begin(), sortedLeft.end(), sortedRight.begin(), sortedRight.end(), std::back_inserter(sorted));
+
+    nums.clear();  // Clear the original vector
+    // Merge the 2 halves into one vector
+    std::merge(sortedLeft.begin(), sortedLeft.end(), sortedRight.begin(), sortedRight.end(), std::back_inserter(nums));
 }
 
 
@@ -27,15 +30,16 @@ int mergeSort(std::vector<int>& nums, int& duration) {
     auto start = std::chrono::high_resolution_clock::now();
 
     std::vector<int> temp(nums.size());
-    mergeSortRecursive(nums,temp);
+    recursiveCall(nums,temp);
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> difference = end - start;
     duration = static_cast<int>(difference.count() * 1000);
-    if (temp.size() % 2 == 0 )
-    {
+    if (temp.size() % 2 == 0 ){
         return temp[(temp.size() / 2) -1];
-    }else{
+    }
+    else{
         return temp[temp.size() / 2];
     }
 }
+
