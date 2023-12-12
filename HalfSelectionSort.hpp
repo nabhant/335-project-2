8dedc6ae309c7a862a26eb413d62729fe7951662
nabhant <nt142003@gmail.com>
2023-12-12 17:35:30 -0500

    12/12

--- HalfSelectionSort.hpp
+++ HalfSelectionSort.hpp
@@ -8,37 +8,32 @@
      * @param: Integer to store the duration of the sort
      * @return: Median of the array
      */
-    int halfSelectionSort ( std::vector<int>& nums, int& duration )
-    {
-        if (nums.size() > 50000){
-            std::cout <<"Input too large for selection sort"<<std::endl;
-        }
-        else if (nums.size() <= 1){
-            std::cout <<"Input too small for selection sort"<<std::endl;
-        }
-        int i = 0, j = 0, min = 0, median = 0;
+int halfSelectionSort(std::vector<int>& nums, int& duration) {
+    int elements_Sorted = 0;
+    auto start = std::chrono::high_resolution_clock::now();
 
-        if (nums.size() % 2 == 0){
-            median = nums.size() / 2;
-        }
-        else{
-            median = (nums.size() / 2) + 1;
-        }
-        auto start = std::chrono::high_resolution_clock::now();
-        auto median_it = nums.begin() + median;
-        for (auto it = nums.begin(); it != median_it; ++it) {
-            auto min_it = it;
-            for (auto it2 = it + 1; it2 != nums.end(); ++it2) {
-                if (*it2 < *min_it) {
-                    min_it = it2;
+    if (nums.size() > 50000) {
+        std::cout << "The array is too large to sort.";
+    } else {
+        for (auto i = nums.begin(); i != nums.end() && elements_Sorted < (nums.size()/2) + (nums.size()%2); ++i) {
+            auto min = i; // min is the current minimum element
+            for (auto j = i + 1; j != nums.end(); ++j) {
+                if (*j < *min) {
+                    min = j;
                 }
             }
-            if (min_it != it) {
-                std::swap(*it, *min_it);
-            }
+            std::swap(*i, *min);
+            elements_Sorted++;
         }
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> difference = end - start;
-        duration = static_cast<int>(difference.count() * 1000);
-        return nums[nums.size()/2];
     }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> difference = end - start;
+    duration = static_cast<int>(difference.count() * 1000); // Convert to milliseconds
+
+    if (nums.size() % 2 == 0) {
+        return *std::next(nums.begin(), (nums.size() / 2) - 1);
+    } else {
+        return *std::next(nums.begin(), nums.size() / 2);
+    }
+}
