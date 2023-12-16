ae90ab4045a86f0272dd48748d678bbe83a8e12c
nabhant <nt142003@gmail.com>
2023-12-16 18:40:47 -0500

    Testing QS

--- QuickSelect.hpp
+++ QuickSelect.hpp
@@ -3,52 +3,110 @@
 #include <chrono>
 #include <algorithm>
 
-// Helper function to swap the pivot into position
-void selectPivot(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high) {
-    auto mid = low + (high - low) / 2;
-    if (*mid < *low) std::iter_swap(mid, low);
-    if (*high < *low) std::iter_swap(high, low);
-    if (*high < *mid) std::iter_swap(high, mid);
-    std::iter_swap(mid, high); // Move the pivot to the end
+/**
+ * Selects the median of three elements (first, middle, last) in the given range.
+ * 
+ * @param low Iterator pointing to the first element.
+ * @param high Iterator pointing to the last element.
+ * @post The median of the first, middle, and last elements is moved to the position of 'high'.
+ */
+void medianOfThree(std::vector<int>::iterator low, std::vector<int>::iterator high) {
+    auto center = std::distance(low, high) / 2 + low;
+
+    // Swap logic to ensure median of three is at the 'high' position
+    if (*low == *high || *low == *center || *center == *high) {
+        std::iter_swap(low, high);
+    }
+    else {
+        if ((*low < *center && *center < *high) || (*low > *center && *center > *high)) {
+            std::iter_swap(center, high);
+        }
+        if ((*low < *high && *center < *low) || (*low > *high && *center > *low)) {
+            std::iter_swap(low, high);
+        } 
+    }
 }
 
+/**
+ * Partitions the array around a pivot element chosen using the medianOfThree strategy.
+ * 
+ * @param nums The vector of integers to be partitioned.
+ * @param low Iterator pointing to the start of the range to partition.
+ * @param high Iterator pointing to the end of the range to partition.
+ * @return Iterator pointing to the partition pivot.
+ * @post Elements are rearranged so that elements less than the pivot come before it, and elements greater come after.
+ */
 std::vector<int>::iterator hoarePartition(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high) {
-    int pivot = *high;
-    auto i = low - 1;
-    auto j = high;
+    medianOfThree(low, high);  // Choose pivot using median of three strategy
+    auto pivot = high;
+    auto left = low;
+    auto right = pivot - 1;
 
+    // Partitioning loop
     while (true) {
-        do { i++; } while (*i < pivot);
-        do { j--; } while (*j > pivot);
-        if (i >= j) return i;
-        std::iter_swap(i, j);
+        while (*left < *pivot) {
+            ++left;
+        }
+        while (*right > *pivot) {
+            --right;
+        }
+        if (std::distance(left, right) > 0) {
+            std::iter_swap(left, right);
+            ++left;
+            --right;
+        } else {
+            break;
+        }
     }
+
+    std::iter_swap(left, pivot);
+    return left;
 }
 
-int quickSelectHelper(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high, int k) {
-    if (high - low <= 10) {
-        std::sort(low, high + 1);
-        return *(low + k);
+/**
+ * Helper function for the Quick Select algorithm. It recursively selects the median element.
+ * 
+ * @param nums The vector of integers.
+ * @param low Iterator pointing to the start of the range.
+ * @param high Iterator pointing to the end of the range.
+ * @param comp Iterator pointing to the position of the desired element (median).
+ * @return The value of the median element.
+ * @post The array is partially sorted such that the median is placed in its correct position.
+ */
+int quickSelectHelper(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high, std::vector<int>::iterator target) {
+    if (high <= low + 9) {
+        std::sort(low, high + 1);  // Use simple sort for small arrays
+        return *target;
     }
 
-    selectPivot(nums, low, high);
-    auto pivotIndex = hoarePartition(nums, low, high - 1);
+    auto pivot = hoarePartition(nums, low, high);
 
-    int pivotDist = pivotIndex - nums.begin();
-    if (pivotDist == k) {
-        return *pivotIndex;
-    } else if (pivotDist > k) {
-        return quickSelectHelper(nums, low, pivotIndex - 1, k);
-    } else {
-        return quickSelectHelper(nums, pivotIndex, high, k);
+    // Recursive calls to find the median
+    if (pivot > target) {
+        return quickSelectHelper(nums, low, pivot - 1, target);
+    } else if (pivot == target) {
+        return *target;
     }
+    return quickSelectHelper(nums, pivot + 1, high, target);
 }
 
+/**
+ * Finds the median of the given vector using the Quick Select algorithm.
+ * 
+ * @param nums The vector of integers.
+ * @param duration Reference to store the duration of the computation.
+ * @return The median of the vector.
+ * @post The median value of the vector is returned, and the computation duration is recorded.
+ */
 int quickSelect(std::vector<int>& nums, int& duration) {
-    int k = (nums.size() - 1) / 2; // Find the median
     auto start = std::chrono::high_resolution_clock::now();
-    int result = quickSelectHelper(nums, nums.begin(), nums.end() - 1, k);
+    int medianIndex = (std::distance(nums.begin(), nums.end()) % 2 == 0) ? (std::distance(nums.begin(), nums.end()) / 2) : ((std::distance(nums.begin(), nums.end()) / 2) + 1);
+
+    auto comp = nums.begin() + medianIndex - 1;
+    int median = quickSelectHelper(nums, nums.begin(), nums.end() - 1, comp);
+
     auto end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    return result;
+    std::chrono::duration<double> difference = end - start;
+    duration = static_cast<int>(difference.count() * 1000);
+    return median;
 }
--- WorstCaseQuickSelect.hpp
+++ WorstCaseQuickSelect.hpp
@@ -5,7 +5,12 @@
 
 
 std::vector<int> worstCaseQuickSelect (void){
-    std::vector<int> nums {1,1,1,1};
+    std::vector<int> nums(20000);
+    int val = 20000;
+    for (auto it = nums.begin(); it != nums.end(); --it){
+        *it = val;
+        val--;
+    }
     return nums;
 }
   // worstCaseQuickSelect generates a worst-case input for a quickselect that uses median-of-3 partitioning. The input it generates must be of length 20,000, and contain each number from 1-20000 once.
